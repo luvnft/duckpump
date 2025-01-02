4170c0c8b01ab73e1958e841cb4ae26a056bed2d
Reza salehi <reza.sl.rb5@gmail.com>
2025-01-02 18:34:17 +0330

    meme list item

--- Frontend/src/utils/helpers.js
+++ Frontend/src/utils/helpers.js
@@ -47,3 +47,15 @@ export function getCustomRelativeTime(createdAt) {
     }`;
   }
 }
+
+export function formatBigPrice(value) {
+  console.log(value);
+  if (value >= 1e9) {
+    return (value / 1e9).toFixed(2) + "B"; // Billions
+  } else if (value >= 1e6) {
+    return (value / 1e6).toFixed(2) + "M"; // Millions
+  } else if (value >= 1e3) {
+    return (value / 1e3).toFixed(2) + "K"; // Thousands
+  }
+  return value?.toString(); // Smaller values
+}
